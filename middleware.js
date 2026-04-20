38211e85cd37e95e07268d7495b01814728efcd1
haruigniter <haru.igniter@gmail.com>
2026-04-20 13:00:15 +0900

    Fix middleware matcher

--- middleware.js
+++ middleware.js
@@ -8,5 +8,5 @@ export default function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/index.html', '/apply/:path*', '/approve/:path*', '/reception/:path*'],
+  matcher: ['/', '/index.html', '/apply/:path*', '/approve/:path*', '/reception/:path*'],
 };
